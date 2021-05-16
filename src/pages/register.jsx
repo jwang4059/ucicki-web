e16666e5c317c642215bcde18b3529f844d8deba
jwang0110 <johnw7@uci.edu>
2021-05-15 17:07:14 -0700

    added registration form

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,20 +1,48 @@
 import React from "react";
-import { Formik, Field, Form } from "formik";
+import { Formik, Form, useField } from "formik";
+import * as Yup from "yup";
 import Layout from "../components/layout";
 
-const TextField = ({ id, name, label, placeholder }) => (
-	<Field name={name}>
-		{({ field, form }) => (
-			<div>
-				<label htmlFor={id}>{label}</label>
-				<input {...field} id={id} placeholder={placeholder} />
-				<span>{form.errors.name}</span>
-			</div>
-		)}
-	</Field>
-);
+const Input = ({ label, ...props }) => {
+	const [field, meta] = useField(props);
+	return (
+		<div className="flex flex-col m-2">
+			<label htmlFor={props.id || props.name}>{label}</label>
+			<input {...field} {...props} />
+			{meta.touched && meta.error ? <div>{meta.error}</div> : null}
+		</div>
+	);
+};
+
+const RegisterFormValidation = Yup.object({
+	firstName: Yup.string()
+		.max(15, "Must be 15 characters or less")
+		.required("Required"),
+	lastName: Yup.string()
+		.max(20, "Must be 20 characters or less")
+		.required("Required"),
+	dob: Yup.date().required("Required"),
+	phone: Yup.string().matches(
+		/^[1-9]\d{2}-\d{3}-\d{4}/,
+		"Invalid phone number format"
+	),
+	email: Yup.string().email("Invalid email address").required("Required"),
+	password: Yup.string()
+		.min(8, "Must be at least 8 characters")
+		.required("Required"),
+	passwordConfirmation: Yup.string()
+		.min(8, "Must be at least 8 characters")
+		.oneOf([Yup.ref("password"), null], "Passwords must match"),
+});
 
 const RegisterPage = () => {
+	const onSubmit = (values, { setSubmitting }) => {
+		setTimeout(() => {
+			alert(JSON.stringify(values, null, 2));
+			setSubmitting(false);
+		}, 400);
+	};
+
 	return (
 		<Layout>
 			<h1>Sign Up</h1>
@@ -22,34 +50,55 @@ const RegisterPage = () => {
 				initialValues={{
 					firstName: "",
 					lastName: "",
+					dob: "",
+					phone: "",
 					email: "",
+					password: "",
+					passwordConfirmation: "",
 				}}
-				onSubmit={async (values) => {
-					await new Promise((r) => setTimeout(r, 500));
-					alert(JSON.stringify(values, null, 2));
-				}}
+				validationSchema={RegisterFormValidation}
+				onSubmit={onSubmit}
 			>
-				<Form className="flex flex-col">
-					<div>
-						<label htmlFor="firstName">First Name: </label>
-						<Field id="firstName" name="firstName" placeholder="Peter" />
+				<Form autoComplete="off">
+					<Input
+						label="First Name: "
+						name="firstName"
+						type="text"
+						placeholder="Peter"
+					/>
+					<Input
+						label="Last Name: "
+						name="lastName"
+						type="text"
+						placeholder="Anteater"
+					/>
+					<Input label="Date of Birth: " name="dob" type="date" />
+					<Input
+						label="Phone Number: "
+						name="phone"
+						type="tel"
+						placeholder="510-123-4567"
+					/>
+					<Input
+						label="Email Address: "
+						name="email"
+						type="email"
+						placeholder="petr@uci.edu"
+					/>
+					<Input label="Password: " name="password" type="password" />
+					<Input
+						label="Confirm Password: "
+						name="passwordConfirmation"
+						type="password"
+					/>
+					<div className="text-center">
+						<button
+							className="bg-blue-700 text-white rounded-sm shadow-sm px-4 py-2 m-2"
+							type="submit"
+						>
+							Submit
+						</button>
 					</div>
-
-					<div>
-						<label htmlFor="lastName">Last Name: </label>
-						<Field id="lastName" name="lastName" placeholder="Anteater" />
-					</div>
-
-					<div>
-						<label htmlFor="email">Email: </label>
-						<Field
-							id="email"
-							name="email"
-							placeholder="petey@uci.edu"
-							type="email"
-						/>
-					</div>
-					<button type="submit">Submit</button>
 				</Form>
 			</Formik>
 		</Layout>
