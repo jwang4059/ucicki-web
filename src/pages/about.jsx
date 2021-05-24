292f0c79e2d0c77e2d132e0d67dbd19544b9ba2b
jwang0110 <johnw7@uci.edu>
2021-05-23 21:46:48 -0700

    updated about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,8 +1,119 @@
 import React from "react";
 import Layout from "../components/layout";
 
+const Tenet = ({ title, text, imageSrc, imageAlt }) => (
+	<section className="flex flex-col justify-center items-center p-6">
+		<h3 className="text-xl font-semibold">{title}</h3>
+		<img className="w-1/3" src={imageSrc} alt={imageAlt || title || ""} />
+		<p>{text}</p>
+	</section>
+);
+
+const Branch = ({ title, text }) => (
+	<section className="flex flex-col justify-center items-center p-6 my-4">
+		<h2 className="text-2xl font-bold">{title}</h2>
+		<p>{text}</p>
+	</section>
+);
 const AboutPage = () => {
-	return <Layout>About Page</Layout>;
+	return (
+		<Layout>
+			<section className="relative md:-mt-24">
+				<div className="bg-black h-full w-full absolute z-10 bg-opacity-50 flex justify-center items-center">
+					<h1 className="text-white text-4xl md:text-6xl text-center font-extrabold">
+						About Circle K
+					</h1>
+				</div>
+				<img
+					className="max-h-screen w-full object-cover"
+					src="https://i.imgur.com/3C2TLj9.jpg"
+					alt=""
+				/>
+			</section>
+			<section className="flex justify-center p-6">
+				<div className=" max-w-6xl">
+					<h2 className="text-xl font-bold">About Circle K International</h2>
+					<p>
+						Circle K International (CKI) is the premier collegiate and
+						university community service, leadership development, and friendship
+						organization in the world. With more than 13,770 members in 17
+						nations, CKI is making a positive impact on the world every day.
+					</p>
+				</div>
+			</section>
+			<div className="md:flex md:justify-center md:items-center">
+				<div className="md:max-w-6xl md:grid md:grid-cols-12 md:gap-4">
+					<article className="bg-blue-200 md:col-span-5">
+						<section className="flex flex-col justify-center items-center p-6">
+							<img
+								className="w-1/3"
+								src="http://www.ucicirclek.com/public/css/cki_emblem.png"
+								alt="Circle K International Emblem"
+							/>
+							<h2 className="text-xl font-bold">The 3 Tenets of Circle K</h2>
+						</section>
+						<Tenet
+							title="Service"
+							text="As one of the three tenets, service is the fundamental element of Circle K. Through service, we are making the world a better place one service project at a time. Together, Circle K truly makes a difference in the community."
+							imageSrc="https://i.imgur.com/IuIosul.png"
+						/>
+						<Tenet
+							title="Leadership"
+							text="Through the mission and vision of the organization, Circle K is
+						dedicated to the realization of humankind’s potential. The unique
+						experiences offered by Circle K truly develop and bring out the
+						leader within each individual."
+							imageSrc="https://i.imgur.com/BC76JTV.png"
+						/>
+						<Tenet
+							title="Fellowship"
+							text="If there is one thing that Circle K knows best, it is fellowship.
+						With each element and aspect of Circle K , members experience
+						genuine fellowship and develop life-long relationships with fellow
+						collegians, advisors, Kiwanians, and citizens in their communities."
+							imageSrc="https://i.imgur.com/k2SzIgT.png"
+						/>
+					</article>
+					<article className="bg-yellow-100 md:col-span-7 flex flex-col justify-center">
+						<section className="flex flex-col justify-center items-center p-6 my-4">
+							<h2 className="text-2xl font-bold">UCI Circle K</h2>
+							<p>
+								UCI Circle K is proud to be the largest Circle K club in the
+								world with over 400 paying members. Last year, we completed over
+								35,000 hours of service, leadership, and fellowship. Our weekly
+								service projects include walking dogs at our local animal
+								shelter, tutoring high school kids after class, and cleaning
+								books ready to be donated to children in need. We also attend
+								various local events throughout the year, including volunteering
+								at OC Night Market, Disneyland, and Santa Ana Zoo. For more
+								information about our events, click on the tabs above or visit
+								our <span className="text-blue-700">calendar</span>!
+							</p>
+						</section>
+						<Branch
+							title="Citrus Division"
+							text="UCI Circle K is part of the Citrus Division of Circle K clubs that encompasses CSU Fullerton, Chapman University, Golden West College, Irvine Valley College, Orange Coast College, Saddleback College, Santa Ana College, Soka University of America, and Whittier College Circle K's. This makes Citrus one of the largest divisions in the CNH District in terms of both the number of clubs and number of members."
+						/>
+						<Branch
+							title="CNH District"
+							text="The Citrus Division, and by extension UCI Circle K, is a part of the CNH District Circle K, which encompasses all 9 Circle K divsions found across California, Nevada, and Hawaii. The other 8 divisons are Capital, Central Coast, Desert Oasis, Foothill, Golden Gate, Metro, Paradise, and Sunset. Every year, all 9 divisions get together for several district-wide events, including Fall Training Conference and DLSSP."
+						/>
+						<Branch
+							title="Kiwanis International"
+							text="Kiwanis International is where everything comes together. Every Key Club, KIWINS, and Circle K are a part of the Kiwanis Family. Kiwanis focuses on changing the world through acts of service and accomplishes its goals through watching over its leadership service programs like Circle K."
+						/>
+					</article>
+				</div>
+			</div>
+			<section className="flex flex-col justify-center items-center p-6">
+				<h2 className="text-2xl font-bold">How Do I Join UCI Circle K?</h2>
+				<p>
+					Complete the <span className="text-blue-700">online application</span>
+					! We look forward to meeting you!
+				</p>
+			</section>
+		</Layout>
+	);
 };
 
 export default AboutPage;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,47 @@
 import React from "react";
 import Layout from "../components/layout";
+import cn from "classnames";
+
+const Section = ({
+	title,
+	text,
+	imageSrc,
+	imageAlt,
+	textColor = "text-black",
+	bgColor = "bg-white",
+	reverse = false,
+}) => {
+	const contentDirection = !reverse ? "md:flex-row" : "md:flex-row-reverse";
+	const textMargin = !reverse ? "md:mr-6" : "md:ml-6";
+
+	return (
+		<section className={cn("flex justify-center p-6", textColor, bgColor)}>
+			<div
+				className={cn(
+					"flex flex-col justify-center w-full max-w-6xl",
+					contentDirection
+				)}
+			>
+				<div
+					className={cn(
+						"flex-grow flex flex-col justify-center mb-6",
+						textMargin
+					)}
+				>
+					<h2 className="text-2xl font-bold">{title}</h2>
+					<p className="text-lg my-2">{text}</p>
+				</div>
+				<div className="flex-shrink-0 flex justify-center items-center md:w-1/2">
+					<img
+						className="w-full max-w-xl object-cover"
+						src={imageSrc}
+						alt={imageAlt}
+					/>
+				</div>
+			</div>
+		</section>
+	);
+};
 
 const IndexPage = () => {
 	return (
@@ -17,87 +59,69 @@ const IndexPage = () => {
 						alt=""
 					/>
 				</section>
+				<section className="text-center p-6">
+					<p className="text-lg">
+						UCI Circle K is a community service organization that focuses on the
+						following 3 tenets: Service, Leadership, and Fellowship.
+					</p>
+				</section>
 				<section className="flex justify-center p-6">
 					<div className="max-w-6xl">
-						<p className="text-lg">
-							UCI Circle K is a community service organization that focuses on
-							the following 3 tenets: Service, Leadership, and Fellowship.
-						</p>
-					</div>
-				</section>
-				<section className="p-6">
-					<h2 className="text-2xl font-bold">What do we do?</h2>
-					<p className="text-lg my-2">
-						As one of the largest club on campus, we offer a little bit of
-						everything for everyone. Whether you're trying to meet new people,
-						learn new skills, or help out the community, we have events,
-						workshops, and socials to help you do just that. Feel free to
-						explore our website and check out our events if you are interested
-						and want to learn more.
-					</p>
-				</section>
-				<section className="flex flex-col text-white bg-blue-800 p-4">
-					{/* Family */}
-					<div className="flex-grow">
-						<h2 className="text-2xl font-bold">Find a home away from home</h2>
+						<h2 className="text-2xl font-bold">What do we do?</h2>
 						<p className="text-lg my-2">
-							Meeting new people can be challenging, especially at such a large
-							school. To help you fit in and find people you click with easier,
-							UCI Circle K offers 13 different families that you can join. Each
-							family has a different feel and its own unique quirks.
+							As one of the largest club on campus, we offer a little bit of
+							everything for everyone. Whether you're trying to meet new people,
+							learn new skills, or help out the community, we have events,
+							workshops, and socials to help you do just that. Feel free to
+							explore our website and check out our events if you are interested
+							and want to learn more.
 						</p>
 					</div>
-					<div className="flex-shrink-0">
-						<img
-							className="w-full"
-							src="https://lh3.googleusercontent.com/pw/ACtC-3dMdzdbM1PsaRbUlFHOwuUppfa-9paH_RF13CQtpWtK92M11gQ4GUZByhzwWsr9K9o7-Zn5Gp-KIrzoka2u0wlJIYJ0MGPwzdHOf6vrNJveEVir2p3TBeBSg_WKT1-mOEEQYT66LyXEjmnWSb3_xbvZ=w1250-h938-no?authuser=0"
-							alt="Fam Pic"
-						/>
-					</div>
 				</section>
-				<section className="flex flex-col p-4">
-					{/* Commmittees */}
-					<div className="flex-grow">
-						<h2 className="text-2xl font-bold">Develop and Grow</h2>
-						<p className="text-lg my-2">Blah Blah Blah join committees!!!</p>
-					</div>
-					<div className="flex-shrink-0">
-						<img
-							className="w-full"
-							src="https://i.imgur.com/sTh5PNL.jpg"
-							alt="Fam Pic"
-						/>
-					</div>
-				</section>
-				<section className="flex flex-col bg-green-200 p-4">
-					{/* Service */}
-					<div className="flex-grow">
-						<h2 className="text-2xl font-bold">Give back to the community</h2>
+				<Section
+					title="Find a home away from home"
+					text="Meeting new people can be challenging, especially at such a
+								large school. To help you fit in and find people you click with
+								easier, UCI Circle K offers 13 different families that you can
+								join. Each family has a different feel and its own unique
+								quirks."
+					imageSrc="https://lh3.googleusercontent.com/pw/ACtC-3dMdzdbM1PsaRbUlFHOwuUppfa-9paH_RF13CQtpWtK92M11gQ4GUZByhzwWsr9K9o7-Zn5Gp-KIrzoka2u0wlJIYJ0MGPwzdHOf6vrNJveEVir2p3TBeBSg_WKT1-mOEEQYT66LyXEjmnWSb3_xbvZ=w1250-h938-no?authuser=0"
+					imgAlt="Fam Pic"
+					textColor="text-white"
+					bgColor="bg-blue-700"
+				/>
+				<Section
+					title="Develop and Grow"
+					text="Blah Blah Blah join committees!!!"
+					imageSrc="https://i.imgur.com/sTh5PNL.jpg"
+					imgAlt="Service Pic"
+					textColor="text-black"
+					bgColor="bg-white"
+					reverse
+				/>
+				<Section
+					title="Give back to the community"
+					text="UCI Circle K is a community service organization. This means we
+								offer weekly service projects for members who want to give back
+								to the community. You are NOT obligated to attend ANY service
+								events, but it is definitely a worth while experience and a good
+								way to meet new people."
+					imageSrc="https://lh3.googleusercontent.com/pw/ACtC-3fMExUl82zv2EmdeGkxYohJnsyKNQ04c69M3t7MHMJvlFwa7UW1BZOBA-NJFCA39_E4eJ9hmI50QvyW0xx1wnAvgf9AUH47NIDDBBdcuNnpZo9Ll4Bl1zwo23noHQ-q_JNDqFYHMnUIJIZt7EUDeCM4=w1250-h938-no?authuser=0"
+					imgAlt="Service Pic"
+					textColor="text-white"
+					bgColor="bg-green-600"
+				/>
+				<section className="flex justify-center p-6">
+					<div className="max-w-6xl">
+						<h2 className="text-2xl font-bold">Join UCI CKI today!</h2>
 						<p className="text-lg my-2">
-							UCI Circle K is a community service organization. This means we
-							offer weekly service projects for members who want to give back to
-							the community. You are NOT obligated to attend ANY service events,
-							but it is definitely a worth while experience and a good way to
-							meet new people.
+							If you are ready to join, fill out the registration form. Feel
+							free to explore the website to learn more about us! If you have
+							any questions, contact a board member or family head. For any
+							concerns about the website please contact the Technology chairs
+							Steven Lam and Thomas Reynolds.
 						</p>
 					</div>
-					<div className="flex-shrink-0">
-						<img
-							className="w-full"
-							src="https://lh3.googleusercontent.com/pw/ACtC-3fMExUl82zv2EmdeGkxYohJnsyKNQ04c69M3t7MHMJvlFwa7UW1BZOBA-NJFCA39_E4eJ9hmI50QvyW0xx1wnAvgf9AUH47NIDDBBdcuNnpZo9Ll4Bl1zwo23noHQ-q_JNDqFYHMnUIJIZt7EUDeCM4=w1250-h938-no?authuser=0"
-							alt=""
-						/>
-					</div>
-				</section>
-				<section className="p-6">
-					<h2 className="text-2xl font-bold">Join UCI CKI today!</h2>
-					<p className="text-lg my-2">
-						If you are ready to join, fill out the registration form. Feel free
-						to explore the website to learn more about us! If you have any
-						questions, contact a board member or family head. For any concerns
-						about the website please contact the Technology chairs Steven Lam
-						and Thomas Reynolds.
-					</p>
 				</section>
 			</article>
 		</Layout>
